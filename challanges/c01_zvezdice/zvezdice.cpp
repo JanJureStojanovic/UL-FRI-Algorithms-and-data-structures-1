5034f2012e99af1ead4d4a6e3d6869807f2dd2d5
Jan Jure Stojanovič <janjures@gmail.com>
2023-10-12 16:47:11 +0200

    Some work on the zvezdice.cpp exercice

--- challanges/c01_zvezdice/zvezdice.cpp
+++ challanges/c01_zvezdice/zvezdice.cpp
@@ -3,20 +3,17 @@
 #include <algorithm>
 #include <tuple>
 
-std::tuple<int, int []> recursion(int arr[][2], int n, int w, int time, int ) {
-    int[2] arr = {1, 3};
-    return std::make_tuple(10, arr);
-}
+using namespace std;
 
 int main() {
 
     int n, w;
-    std::cin >> n >> w;
+    cin >> n >> w;
 
     int arr[n][2];
 
     for (int i = 0; i < n; i++) {
-        std::cin >> arr[i][0] >> arr[i][1];
+        cin >> arr[i][0] >> arr[i][1];
     }
 
 
