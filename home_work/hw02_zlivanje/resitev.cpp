e4478d6be74f11a33b24b2ca2e90085242ccf481
Jan Jure Stojanovič <janjures@gmail.com>
2023-10-18 17:38:57 +0200

    Vector of vectors done: vector<vector<int>>

--- home_work/hw02_zlivanje/resitev.cpp
+++ home_work/hw02_zlivanje/resitev.cpp
@@ -1,6 +1,3 @@
-// Second home work
-
-
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -17,25 +14,61 @@ int main() {
     // Vector that holds ascending subsets
     vector<vector<int>> cete;
 
-    int x, y;
+    int x, y; // The two neighbouring numbers
     cin >> x;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++) {
 
         vector<int> ceta;
+        cete.push_back(ceta);
 
-        cin >> y;
+        for (int j = i; j < n - 1; j++) {
 
-        ceta.push_back(x)
-        
+            i++; // Moving the i along
 
-        
+            // We scan the next number -> we have two options
+            cin >> y;
+            
+            // 1st option: x <= y
+            if (x <= y) {
+                cete.back().push_back(x);
+                x = y;
+            }
+
+            // 2nd option: x > y
+            if (x > y) {
+                cete.back().push_back(x);
+                x = y;
+                break;
+            }
+        }
+
+        i--; // Decrement of one needed since the for loop adds one
     }
 
-    for (int i = 0; i < cete.size(); i++) {
+    cete.back().push_back(y); // Add the forgotten element
+
+    // Now that we have a vector of vectors we can sort it accordingly
+
+    for (int i = 0; i < a; i++) { // The amount of times we sort the main vector
+
+        for (int j = 0; j < cete.size(); j += k) { // The number of subset vectors we merge
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+    /*for (int i = 0; i < cete.size(); i++) {
         for (int j = 0; j < cete[i].size(); j++) {
             cout << cete[i][j] << " ";
         }
         cout << endl;
-    }
+    }*/
 }
