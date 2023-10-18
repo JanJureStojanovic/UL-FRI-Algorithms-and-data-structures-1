3d93e4a87a94dd47ec9a96fbc2df6957bb282570
Jan Jure Stojanovič <janjures@gmail.com>
2023-10-18 13:58:30 +0200

    Removed bad recursion, needs do over ..

--- challanges/c01_zvezdice/zvezdice.cpp
+++ challanges/c01_zvezdice/zvezdice.cpp
@@ -2,8 +2,11 @@
 #include <string>
 #include <algorithm>
 #include <tuple>
+#include <vector>
 
 using namespace std;
+
+
 
 int main() {
 
@@ -14,8 +17,32 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i][0] >> arr[i][1];
-    }
+    } 
 
-
-           
+      
 }
+
+/* Uporabi vektor, spomni se funkcije podvajanja iz predavanja (to uporabi za izpis array-a 0, 1 in 2)
+
+void izpisi(vector<int> v) {
+    for (int x : v) cout << x << " ";
+    cout << endl;
+}
+
+auto podvoji(vector<int> v) {
+    int n=v.size();
+    for (int i=0; i<n; i++) {
+        v.push_back(v[i]);
+    }
+    return v;
+}
+
+vector<int> a = {1,2,3,4,5};
+vector<int> b = podvoji(a);
+izpisi(a);
+izpisi(b);
+
+Output:
+1 2 3 4 5 
+1 2 3 4 5 1 2 3 4 5 */
+
