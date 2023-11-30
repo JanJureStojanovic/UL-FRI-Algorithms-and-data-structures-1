bd2a0295c51cbbd2b49a4b528dbb3cad33b8918d
Jan Jure Stojanovič <janjures@gmail.com>
2023-11-30 17:08:17 +0100

    Started hw

---
+++ home_work/hw07_ujemanje_vzorca/resitev.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    // Number of pairs
+    int n;
+    cin >> n;
+
+    // Our pairs of strings
+    string s, t;
+
+    // Iteration over n amount of pairs
+    for (int i = 0; i < 5; i++) {
+
+        // Read the pairs
+        cin >> s >> t;
+
+        // For loop matching the sample string to the main string
+        for (int j = 0; j < t.length() - s.length(); j++) {
+
+        }
+
+        // Compare the pairs
+
+
+    }
+
+    
+
+}
