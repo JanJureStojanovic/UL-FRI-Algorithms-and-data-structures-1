c8b54581996fa1967d2b3e416bb4c08d38c87b2f
Jan Jure Stojanovič <janjures@gmail.com>
2023-12-01 15:50:39 +0100

    Still not done :(

--- home_work/hw07_ujemanje_vzorca/resitev.cpp
+++ home_work/hw07_ujemanje_vzorca/resitev.cpp
@@ -5,80 +5,94 @@
 
 using namespace std;
 
+pair<int, int> ujemanje (string vzorec, string niz);
+
 int main() {
 
-    // Number of pairs
-    int st_parov;
+    //pair<int, int> primer = ujemanje("jan", "jurejan");
+    //cout << primer.first << primer.second;
+
+    int st_parov; // Number of pairs
     cin >> st_parov;
 
-    // Our pairs of strings
-    string vzorec, niz;
+    string vzorec, niz; // Our pairs of strings
 
-    // Program end output (it will hold n pairs)
-    vector<pair<int, int>> indeks_output;
+    vector<pair<int, int>> indeks_output; // Program end output (it will hold n pairs)
 
     // Iteration over n amount of pairs
     for (int i = 0; i < 5; i++) {
 
-        // Read the pairs
-        cin >> vzorec >> niz;
+        cin >> vzorec >> niz; // Read the pairs
 
-        // Compare the pairs
         // For loop matching the sample string to the main string
-        for (int j = 0; j < niz.length() - vzorec.length(); j++) {
+        for (int j = 0; j < niz.length() - vzorec.length(); j++) { 
 
-            // Index going over the main string
-            int l = j;
-
-            // Index if we found mathching sample
-            int zacetni_indeks = j;
+            int zacetni_indeks = j; // Index if we found mathching sample
             int koncni_indeks;
 
-            // Bool to check if we found matching sample
-            bool ujemanje_nizov = true;
+            bool ujemanje_nizov = true; // Bool to check if we found matching sample
 
-            for (int k = 0; k < vzorec.lenght(); k++) {
+            // Compare the pairs (for each char in vzorec)
+            for (int k = 0; k < vzorec.length(); k++) { 
 
-                // If '?' we can move on
-                if (vzorec[k] = '?') {
-                    l++;
+                // Ni ujemanja -> gremo naprej po nizu
+                if (vzorec[k] != niz[k + j]) {
+                    ujemanje_nizov = false;
+                    break;
+                }
+
+                // If '?' we can move on (imamo ujemanje)
+                if (vzorec[k] = '?') { 
                     continue;
                 }
 
                 // We have to check all possibilities
                 if (vzorec[k] = '*') {
 
-                    /* Najprej probamo ce je lahko '*' prazen niz
-                    Ce ne gre, nadaljujemo do prvega ujemanja (premikamo l, dokler k + 1 ni enak l)
-                    Ce je vec ujemanj, moramo porobati vse (dvojni loop) */ 
+                    int x = k + 1; // Vzamemo naslednjega za zvezdico
+                    int y = j; // Vzamemo l-ti znak (kjer smo ostali)
 
-                    int a = l;
+                    // Najdemo niz, ki pride za '*' in iscemo ujemanje
+                    while (y < vzorec.length()) {
 
-                    // Premikamo se po nizu in iscemo ujemanje z znakom za '*'
-                    while (a < niz.length() - vzorec.length()) { 
 
-                        // Najdemo ujemanje znakov
-                        if (vzorec[k+1] == niz[a]) {
 
-                            // Nadaljujemo kot smo zaceli
-
-                        }
-
-                        // Premik po nizu
-                        a++;
+                        y++;
 
                     }
                 }
 
-                // Ni ujemanja -> gremo naprej po nizu
-                if (vzorec[k] != niz[l]) {
-                    
-                    break;
-                }
-
-                l++;
+                k++; // Premik indeksa
 
             }
         }
     }
+}
+
+// Funkcija, ki najde prvo ujemanje (brez upostevanja '?' in '*')
+pair<int, int> ujemanje(string vzorec, string niz) {
+
+    int a, b;
+
+    for (int i = 0; i < niz.length() - vzorec.length() + 1; i++) {
+
+        a = i;
+        b = i + vzorec.length() - 1;
+
+        bool ujemanje = true;
+
+        for (int j = 0; j < vzorec.length(); j++) {
+            
+            if (vzorec[j] != niz[i + j]) {
+                ujemanje = false;
+                break;
+            }
+        }
+
+        if (ujemanje == true) {
+            return make_pair(a, b);
+        }
+    }
+
+    return make_pair(-1, -1);
 }
