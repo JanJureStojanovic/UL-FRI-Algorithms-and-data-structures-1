bc85484a7d21d9d4563cb4ed1b1ce72f1e050fb1
Jan Jure Stojanovič <janjures@gmail.com>
2023-12-04 22:21:20 +0100

    Started with new hw

---
+++ home_work/hw08_razporeditev/resitev.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    // Stevilo otrok in stevilo parov
+    int n, m;
+    cin >> n >> m;
+
+    // Nas data structure
+    vector<int, vector<int>> skupina_in_prijatelji;
+
+    // Preberemo m mnogo prijateljstev
+    for (int i = 0; i < m; i++) {
+
+    }
+
+}
