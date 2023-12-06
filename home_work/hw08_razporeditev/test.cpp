cffba78497e98e7ffb13ff3141764d6522382fdd
Jan Jure Stojanovič <janjures@gmail.com>
2023-12-06 12:34:51 +0100

    Works on small cases

--- home_work/hw08_razporeditev/test.cpp
+++ home_work/hw08_razporeditev/test.cpp
@@ -9,11 +9,16 @@ using namespace std;
 
 template<typename T>
 void print(vector<T> sez) {
-    for (T x : sez) cout << x << " ";
-    cout << endl;
+    for (T x : sez) {
+        if (x == 1) {
+            cout << 1 << endl;
+        } else {
+            cout << 2 << endl;
+        }
+    }
 }
 
-void BFS(int x, vector<vector<int>> &adj, vector<int> &vis, vector<int> &seq) {
+int BFS(int x, vector<vector<int>> &adj, vector<int> &vis, vector<int> &seq) {
 
     int kam_gre = 1;
 
@@ -30,11 +35,23 @@ void BFS(int x, vector<vector<int>> &adj, vector<int> &vis, vector<int> &seq) {
         seq.push_back(x);
         for (int y : adj[x]) if (vis[y]==0) {
             q.push(y); 
+
+            // Preverimo ce gre lahko sem
+
+            for (int a : adj[y]) {
+
+                if (vis[a] == kam_gre) {
+                    return 1;
+                }
+            }
+
             vis[y] = kam_gre;
         }
 
         kam_gre *= -1;
     }
+
+    return 0;
 }
 
 int main() {
@@ -47,21 +64,32 @@ int main() {
     for (int i = 0; i < m; i++) {
         int a,b;
         cin >> a >> b;
-        sosedi[a].push_back(b);
-        sosedi[b].push_back(a);
+        sosedi[a-1].push_back(b-1);
+        sosedi[b-1].push_back(a-1);
     }
 
+    /* 
     for (int x = 0; x < n; x++) {
         cout << x << ": ";
         print(sosedi[x]);
-    }
+    }*/
+
+    int p;
     
     vector<int> vis(n);
     for (int i = 0; i < n; i++) { 
         if (vis[i] == 0) { 
             vector<int> seqB;
-            BFS(i, sosedi, vis, seqB);
-            print(seqB);    
+            p = BFS(i, sosedi, vis, seqB);
+
+            if (p == 1) {
+                break;
+            } 
         }
+    }
+    if (p == 1) {
+        cout << -1 << endl;
+    } else {
+        print(vis);
     }
 }
