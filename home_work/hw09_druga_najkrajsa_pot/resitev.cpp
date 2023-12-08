624f142695cab46cd2b9aa14767844d98caa5187
Jan Jure Stojanovič <janjures@gmail.com>
2023-12-08 12:55:14 +0100

    Weighted graph scan

--- home_work/hw09_druga_najkrajsa_pot/resitev.cpp
+++ home_work/hw09_druga_najkrajsa_pot/resitev.cpp
@@ -9,5 +9,17 @@ using namespace std;
 
 int main() {
 
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<pair<int, int>>> adjw(n);
+
+    for (int i = 0; i < m; i++) {
+        int a, b, w;
+        cin >> a >> b >> w;
+        adjw[a].push_back({b,w});
+        adjw[b].push_back({a,w});
+    }
+    
     return 0;
 }
