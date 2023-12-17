75bc59c16e34d5bfa1a267e6eab26f58f7b53da4
Jan Jure Stojanovič <janjures@gmail.com>
2023-12-17 12:49:44 +0100

    shortest_path method added

--- home_work/hw09_druga_najkrajsa_pot/resitev.cpp
+++ home_work/hw09_druga_najkrajsa_pot/resitev.cpp
@@ -17,77 +17,43 @@ void print(const vector<T> &sez) {
     cout << endl;
 }
 
-void removeEdge(vector<vector<pair<int, int>>>& graph, int u, int v) {
-    // Iterate over each vertex and remove the edge (u, v) and (v, u)
-    for (auto& neighbors : graph) {
-        neighbors.erase(remove_if(neighbors.begin(), neighbors.end(),
-                                  [u, v](const pair<int, int>& edge) {
-                                      return (edge.first == u && edge.second == v) ||
-                                             (edge.first == v && edge.second == u);
-                                  }),
-                        neighbors.end());
-    }
-}
 
-
-vector<VII> remove_edge(vector<VII> adjw, pair<int, int> edge) {
-
-    int x = edge.first;
-    int y = edge.second;
-
-    for(int i = 0; i < adjw[x].size(); i++) {
-        if (adjw[x][i].first == y) {
-            adjw[x].erase(adjw[x].begin() + i);
-            break;
-        }
-    }
-
-    for(int i = 0; i < adjw[y].size(); i++) {
-        if (adjw[y][i].first == x) {
-            adjw[y].erase(adjw[y].begin() + i);
-            break;
-        }
-    }
-
-    return adjw;
-}
-
-// Method that returns the edges that complete the shortest path 
-vector<pair<int, int>> path_finder(vector<int> prev, int n) {
-
-    // Vektor, ki hrani povezave, ki tvorijo naso pot
-    vector<pair<int, int>> pot;
-    int a = n - 1;
-
-    while (a != 0) {
-        pot.push_back(make_pair(prev[a], a));
-        a = prev[a];
-    }
-
-    return pot;
-}
-
-void Dijkstra_PQ(vector<VII> &adjw, int start, vector<int> &dist, vector<int> &prev) {
+// Dijkstra shortest path algoritm
+void dijkstra_PQ(vector<VII> &adjw, int start, vector<int> &dist, vector<int> &prev) {
 
     int n = adjw.size();
     dist = vector<int> (n,-1); 
     prev = vector<int> (n,-1);
-    priority_queue<PII, vector<PII>, greater<PII>> pq;  // (distance, node)
+    priority_queue<PII, vector<PII>, greater<PII>> pq;
     dist[start] = 0; 
     pq.push({0,start});
 
     while (!pq.empty()) {
         auto [d,x] = pq.top(); 
         pq.pop();
-        if (dist[x] != d) continue;  // ignore old values
-        for (auto [y,w] : adjw[x]) {  // update neighbors
+        if (dist[x] != d) continue;
+        for (auto [y,w] : adjw[x]) {
             int d = dist[x]+  w;
-            if (dist[y]==-1 || d<dist[y]) {
-                dist[y]=d; prev[y]=x;
+            if (dist[y] == -1 || d < dist[y]) {
+                dist[y] = d; 
+                prev[y] = x;
                 pq.push({d,y});
             }
         }
     }
+}
+
+// Method that returns a vector of consecutive nodes that create the shortest path
+vector<int> shortest_path(vector<int> prev, int n) {
+
+    vector<int> shortest_path_vector;
+    int i = n-1;
+    while(i > 0) {
+        shortest_path_vector.push_back(i);
+        i = prev[i];
+    }
+    shortest_path_vector.push_back(i);
+    return shortest_path_vector;
 }
 
 int main() {
@@ -105,34 +71,12 @@ int main() {
     }
 
     vector<int> dist, prev;
-    Dijkstra_PQ(adjw,0,dist,prev);
-    //print(dist); 
-    //print(prev);
+    dijkstra_PQ(adjw,0,dist,prev);
 
-    vector<pair<int, int>> pot = path_finder(prev, n);
+    vector<int> shortest_path_edges = shortest_path(prev, n);
 
-    int min_pot = 20000000;
-
-    for (int i = 0; i < pot.size(); i++) {
-
-        int attempt = 0;
-
-        vector<VII> altered_adjw = adjw;
-        removeEdge(altered_adjw, pot[i].first, pot[i].second);
-
-        vector<int> dist1, prev1;
-
-        Dijkstra_PQ(altered_adjw,0,dist1,prev1);
-
-
-        attempt = dist[n-1];
-
-        if (attempt < min_pot) {
-            min_pot = attempt;
-        }
-    }
-
-    cout << min_pot << "\n";
+    print(shortest_path_edges);
 
     return 0;
+  
 }
