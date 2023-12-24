a2fce0d47ee6ee8c9c86d150f0aee562b31f1128
Jan Jure Stojanovič <janjures@gmail.com>
2023-12-24 14:24:26 +0100

    Finding the levels

--- home_work/hw10_otoki/resitev.cpp
+++ home_work/hw10_otoki/resitev.cpp
@@ -43,11 +43,39 @@ public:
         parent[x] = y;  // attach to larger root
         size[y] += size[x];
     }
+
+    
 };
 
 
 int main() {
 
+    int v, s;
+    cin >> v >> s;
+
+    int t;
+
+    int max = 0;
+
+    vector<VII> matrix(100000);
+
+    for (int i = 0; i < v; i++) {
+        for (int j = 0; j < s; j++) {
+            cin >> t;
+            matrix[t].push_back({i,j});
+            if (t > max) {
+                max = t;
+            }
+        }
+    }
+
+    for (int i = 0; i < max; i++) {
+        cout << i << ": ";
+        for (int j = 0; j < matrix[i].size(); j++) {
+            cout << "(" << matrix[i][j].first << ", " << matrix[i][j].second << ") ";
+        }
+        cout << "\n";
+    }
 
     return 0;
 }
